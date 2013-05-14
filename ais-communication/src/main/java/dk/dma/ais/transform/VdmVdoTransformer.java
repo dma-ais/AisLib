563d850e110448e1d8ccefca5a31a5061173b67e
Ole Borup <obo@frv.dk>
2013-05-14 14:43:15 +0200

    Added option to do talker transform for own messages

--- ais-communication/src/main/java/dk/dma/ais/transform/VdmVdoTransformer.java
+++ ais-communication/src/main/java/dk/dma/ais/transform/VdmVdoTransformer.java
@@ -29,18 +29,24 @@ import dk.dma.ais.sentence.Sentence;
 import dk.dma.ais.sentence.SentenceException;
 
 /**
- * Transformer that changes VDM/VDO to either VDM or VDO based
- * on a given MMSI
+ * Transformer that changes VDM/VDO to either VDM or VDO based on a given MMSI. If talker is given the talker is also changed for
+ * own messages.
  */
 @ThreadSafe
 public class VdmVdoTransformer implements IAisPacketTransformer {
-    
+
     private static final Pattern VDM_PATTERN = Pattern.compile("!..VD(M|O)", Pattern.DOTALL);
-    
+
     private final int ownMmsi;
-    
+    private final String ownTalker;
+
     public VdmVdoTransformer(int ownMmsi) {
+        this(ownMmsi, null);
+    }
+
+    public VdmVdoTransformer(int ownMmsi, String ownTalker) {
         this.ownMmsi = ownMmsi;
+        this.ownTalker = ownTalker;
     }
 
     @Override
@@ -50,15 +56,15 @@ public class VdmVdoTransformer implements IAisPacketTransformer {
             return null;
         }
         boolean own = message.getUserId() == ownMmsi;
-        
+
         // No need to change
         if (message.getVdm().isOwnMessage() == own) {
             return packet;
         }
-        
+
         // Split lines
         List<String> lines = packet.getStringMessageLines();
-        for (int i=0; i < lines.size(); i++) {
+        for (int i = 0; i < lines.size(); i++) {
             // Find where VDO/VDM starts and replace last character
             String line = lines.get(i);
             Matcher matcher = VDM_PATTERN.matcher(line);
@@ -67,24 +73,31 @@ public class VdmVdoTransformer implements IAisPacketTransformer {
             }
             int start = matcher.start();
             line = line.substring(0, start + 5) + (own ? 'O' : 'M') + line.substring(start + 6);
-            
+
+            // Maybe do talker transformation
+            if (own && ownTalker != null) {
+                System.out.println("line before: " + line);
+                line = line.substring(0, start + 1) + ownTalker + line.substring(start + 3);
+                System.out.println("line after: " + line);
+            }
+
             // Calculate checksum
             int checksum;
             try {
                 checksum = Sentence.getChecksum(line.substring(start));
-            } catch (SentenceException e) {                
+            } catch (SentenceException e) {
                 e.printStackTrace();
                 return null;
             }
             String strChecksum = Sentence.getStringChecksum(checksum);
-            
+
             // Find where checksum starts and replace checksum
             start = line.indexOf('*', start);
-            line = line.substring(0, start + 1) + strChecksum + line.substring(start + 3); 
-            
-            lines.set(i, line);             
+            line = line.substring(0, start + 1) + strChecksum + line.substring(start + 3);
+
+            lines.set(i, line);
         }
-        
+
         return AisPacket.from(StringUtils.join(lines, "\r\n"), packet.getReceiveTimestamp());
     }
 
