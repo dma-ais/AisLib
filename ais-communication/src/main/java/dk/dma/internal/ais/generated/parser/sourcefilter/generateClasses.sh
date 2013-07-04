c0ebdd306e721a410feffd9845010164e71aa2d3
Kasper Nielsen <kasperni@gmail.com>
2013-07-04 12:28:18 +0200

    update pom

---
+++ ais-communication/src/main/java/dk/dma/internal/ais/generated/parser/sourcefilter/generateClasses.sh
@@ -0,0 +1,1 @@
+java -cp .:/Users/kasperni/Downloads/antlr-4.1-complete.jar -jar /Users/kasperni/Downloads/antlr-4.1-complete.jar -no-listener -visitor -package dk.dma.internal.ais.generated.parser.sourcefilter SourceFilter.g4
