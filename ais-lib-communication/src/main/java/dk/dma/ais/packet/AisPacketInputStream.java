0513c4ed6297822f1fb772cfa3d00626d0cdaa93
Kasper Nielsen <kasperni@gmail.com>
2013-07-11 13:34:11 +0200

    Better close of AisPacketInputStream

--- ais-lib-communication/src/main/java/dk/dma/ais/packet/AisPacketInputStream.java
+++ ais-lib-communication/src/main/java/dk/dma/ais/packet/AisPacketInputStream.java
@@ -1,17 +1,17 @@
-/*
- * Copyright (c) 2008 Kasper Nielsen.
+/* Copyright (c) 2011 Danish Maritime Authority
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.dma.ais.packet;
 
@@ -43,7 +43,7 @@ public class AisPacketInputStream {
 
     final InputStream stream;
 
-    volatile boolean closed = false;
+    volatile boolean closed;
 
     public static void main(String[] args) throws IOException {
         AisPacketInputStream s = new AisPacketInputStream(new FileInputStream("null"));
@@ -71,8 +71,9 @@ public class AisPacketInputStream {
 
     protected void handleAbk(Abk abk) {}
 
-    public void close() {
-
+    public void close() throws IOException {
+        stream.close();
+        closed = true;
     }
 
     /**
