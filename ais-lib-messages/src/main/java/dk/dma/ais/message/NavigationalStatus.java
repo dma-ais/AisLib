265bc5cf467dbe7e20003816142217fb941b828d
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-03-25 10:29:26 +0100

    Added encoded value of NavigationalStatus to enum.

--- ais-lib-messages/src/main/java/dk/dma/ais/message/NavigationalStatus.java
+++ ais-lib-messages/src/main/java/dk/dma/ais/message/NavigationalStatus.java
@@ -15,40 +15,62 @@
  */
 package dk.dma.ais.message;
 
+/**
+ * Navigational status as used in AisMessages of type 1, 2, and 3 and
+ * defined in Rec. ITU-R M.1371-4, table 45.
+ *
+ *
+ *     0 = under way using engine,
+ *     1 = at anchor,
+ *     2 = not under command,
+ *     3 = restricted manoeuvrability
+ *     4 = constrained by her draught,
+ *     5 = moore
+ *     6 = aground
+ *     7 = engaged in fishing
+ *     8 = under way sailing
+ *     9 = reserved for future amendment of navigational status for ships carrying DG, HS, or MP, or IMO hazard or pollutant category C, high speed craft (HSC),
+ *     10 = reserved for future amendment of navigational status for ships carrying dangerous goods (DG), harmful substances (HS) or marine pollutants (MP), or IMO hazard or pollutant category A, wing in grand (WIG);
+ *     11-13 = reserved for future use,
+ *     14 = AIS-SART (active),
+ *     15 = not defined = default (also used by AIS-SART under test)
+ */
 public enum NavigationalStatus {
 
-    UNDEFINED, UNDER_WAY_USING_ENGINE, AT_ANCHOR, NOT_UNDER_COMMAND, RESTRICTED_MANOEUVRABILITY, CONSTRAINED_BY_HER_DRAUGHT, MOORED, AGROUND, ENGAGED_IN_FISHING, UNDER_WAY, SAILING, AIS_SART {
+    UNDER_WAY_USING_ENGINE(0),
+    AT_ANCHOR(1),
+    NOT_UNDER_COMMAND(2),
+    RESTRICTED_MANOEUVRABILITY(3),
+    CONSTRAINED_BY_HER_DRAUGHT(4),
+    MOORED(5),
+    AGROUND(6),
+    ENGAGED_IN_FISHING(7),
+    UNDER_WAY(8),
+    UNDEFINED(15),
+    AIS_SART(14)
+    {
         public String prettyStatus() {
             return name().replace("_", "-");
-        }}
-    ;
-    public static NavigationalStatus get(int intNavigationalStatus) {
-        switch (intNavigationalStatus) {
-        case 0:
-            return UNDER_WAY_USING_ENGINE;
-        case 1:
-            return AT_ANCHOR;
-        case 2:
-            return NOT_UNDER_COMMAND;
-        case 3:
-            return RESTRICTED_MANOEUVRABILITY;
-        case 4:
-            return CONSTRAINED_BY_HER_DRAUGHT;
-        case 5:
-            return MOORED;
-        case 6:
-            return AGROUND;
-        case 7:
-            return ENGAGED_IN_FISHING;
-        case 8:
-            return UNDER_WAY;
-        case 14:
-            return AIS_SART;
-        case 15:
-            return UNDEFINED;
-        default:
-            return UNDEFINED;
         }
+    };
+
+    private final int code;
+
+    NavigationalStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static NavigationalStatus get(int intNavigationalStatus) {
+        for (NavigationalStatus navigationalStatus : NavigationalStatus.values()) {
+            if (intNavigationalStatus == navigationalStatus.code) {
+                return navigationalStatus;
+            }
+        }
+        return UNDEFINED;
     }
 
     public String prettyStatus() {
