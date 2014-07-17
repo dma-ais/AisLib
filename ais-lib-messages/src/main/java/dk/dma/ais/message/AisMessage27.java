b86db46269fa03b3e2fb5e56153e15752dc75508
Ole Borup <obo@dma.dk>
2014-07-17 22:53:07 +0200

    Set AisMessage27 to implement IPositionMessage and added license

--- ais-lib-messages/src/main/java/dk/dma/ais/message/AisMessage27.java
+++ ais-lib-messages/src/main/java/dk/dma/ais/message/AisMessage27.java
@@ -1,3 +1,17 @@
+/* Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dk.dma.ais.message;
 
 import dk.dma.ais.binary.BinArray;
@@ -11,7 +25,7 @@ import dk.dma.ais.sentence.Vdm;
  * Long-range broadcast position report implemented according to ITU-R M.1371-4
  *
  */
-public class AisMessage27 extends AisMessage {
+public class AisMessage27 extends AisMessage implements IPositionMessage {
 
     /** serialVersionUID. */
     private static final long serialVersionUID = 1L;
