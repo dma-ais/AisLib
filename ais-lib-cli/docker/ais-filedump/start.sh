2412c20e3d1d9b9435d2f95842f256cad22d0c44
Jens Tuxen <mail@jenstuxen.com>
2014-12-03 13:08:18 +0100

    Fix directory environment variable + echo run command

--- ais-lib-cli/docker/ais-filedump/start.sh
+++ ais-lib-cli/docker/ais-filedump/start.sh
@@ -1,2 +1,4 @@
 JAR=`ls /archive/ais-lib-cli/target/ais-lib-cli*SNAPSHOT.jar`
+echo "Running: "
+echo "java -jar $JAR filedump $SOURCES -directory $DIRECTORY"
 java -jar $JAR filedump $SOURCES -directory $DIRECTORY
