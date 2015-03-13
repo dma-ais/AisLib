f1902386649eef9c8f91234a62c69cbbef4b4546
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-03-13 11:52:10 +0100

    Fixed silly mistake.

--- ais-lib-messages/src/test/java/dk/dma/ais/message/AisMessage7Test.java
+++ ais-lib-messages/src/test/java/dk/dma/ais/message/AisMessage7Test.java
@@ -5,23 +5,16 @@ import dk.dma.ais.sentence.SentenceException;
 import dk.dma.ais.sentence.Vdm;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class AisMessage7Test {
 
-    @Test
+    @Test(expected = SixbitException.class)
     public void test() throws AisMessageException, SixbitException, SentenceException {
         Vdm vdm = new Vdm();
         vdm.parse(
             "!AIVDM,1,1,,A,701uEO1:CovP00,4*6A"
         );
 
-        AisMessage message = AisMessage.getInstance(vdm);
-        assertTrue(message instanceof AisMessage7);
-        AisMessage7 msg7 = (AisMessage7) message;
-        assertEquals(2655135, msg7.getUserId());
-        assertEquals(4, msg7.getMsgId());
+        AisMessage.getInstance(vdm);
     }
 
 }
