4de29947e5853e9339c145de71ea797dd51124f6
Kasper Nielsen <kasperni@gmail.com>
2015-03-27 10:01:07 +0100

    simplify test

--- ais-lib-communication/src/test/java/dk/dma/ais/tracker/TargetTrackerTest.java
+++ ais-lib-communication/src/test/java/dk/dma/ais/tracker/TargetTrackerTest.java
@@ -21,6 +21,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import org.junit.Before;
@@ -28,83 +29,73 @@ import org.junit.Test;
 
 import dk.dma.ais.data.AisTarget;
 import dk.dma.ais.data.AisVesselTarget;
-import dk.dma.ais.message.AisMessage;
 import dk.dma.ais.reader.AisReader;
 import dk.dma.ais.reader.AisReaders;
-import java.util.function.Consumer;
-
 
 public class TargetTrackerTest {
 
     AisReader aisReader;
+
     TargetTracker targetTracker;
-    HashMap<Integer,AisTarget> aisVesselTargets;
+
+    Map<Integer, AisTarget> aisVesselTargets;
 
     @Before
     public void setUp() throws Exception {
         aisVesselTargets = new HashMap<>();
         Path testdata = Paths.get("src/test/resources/");
         aisReader = AisReaders.createDirectoryReader(testdata.toString(), "*.txt", true);
-        
+
         targetTracker = new TargetTracker();
         targetTracker.subscribeToPacketStream(aisReader.stream());
-        
-        aisReader.registerHandler(new Consumer<AisMessage>() {
-            
-            @Override
-            public void accept(AisMessage t) {
-                if (aisVesselTargets.containsKey(t.getUserId())) {
 
-                } else {
-                    try {
-                        AisTarget k = Objects.requireNonNull(AisTarget.createTarget(t));
-                        if (k instanceof AisVesselTarget) {
-                            aisVesselTargets.put(t.getUserId(), k);
-                        }
-                        
-                    } catch(NullPointerException e) {
-                        
-                    } 
+        aisReader.registerHandler(t -> {
+            if (!aisVesselTargets.containsKey(t.getUserId())) {
+                AisTarget k = AisTarget.createTarget(t);
+                if (k instanceof AisVesselTarget) {
+                    aisVesselTargets.put(t.getUserId(), k);
                 }
             }
         });
-        
+
         aisReader.start();
-        //read all first
+        // read all first
         aisReader.join();
-        
-        
+
+
     }
 
     @Test
-    public void testExists() throws IOException, InterruptedException {
-        for (AisTarget t: aisVesselTargets.values()) {
+    public void testExists()  {
+        for (AisTarget t : aisVesselTargets.values()) {
             Objects.requireNonNull(targetTracker.get(Objects.requireNonNull(t.getMmsi())));
         }
     }
-    
+
     /**
      * Test that all AisVesselTargets with names have names in TargetTracker
+     * 
      * @throws IOException
      * @throws InterruptedException
      */
     @Test
     public void testNames() throws IOException, InterruptedException {
         List<String> names = new ArrayList<String>();
-        for (AisTarget t: aisVesselTargets.values()) {
+        for (AisTarget t : aisVesselTargets.values()) {
             if (t instanceof AisVesselTarget) {
                 if (((AisVesselTarget) t).getVesselStatic() != null) {
                     String n = ((AisVesselTarget) t).getVesselStatic().getName();
                     if (n != null && !n.equals("")) {
-                        
+
                         names.add(n);
-                        
-                        assert ((AisVesselTarget)targetTracker.get(t.getMmsi()).getAisTarget()).getVesselStatic().getName() != null;
-                        
-                        
+
+                        assert ((AisVesselTarget) targetTracker.get(t.getMmsi()).getAisTarget()).getVesselStatic()
+                                .getName() != null;
+
+
                     }
                 }
-                
+
             }
         }
 
