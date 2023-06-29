9d331330a59d46a97732ed7e43240301a2d45fff
dimitrispallas <114162301+dimitrispallas@users.noreply.github.com>
2023-06-29 18:24:10 +0300

    Revert "fixed rate of turn value according to ais specification"

--- ais-lib-messages/src/main/java/dk/dma/ais/message/AisPositionMessage.java
+++ ais-lib-messages/src/main/java/dk/dma/ais/message/AisPositionMessage.java
@@ -252,17 +252,7 @@ public abstract class AisPositionMessage extends AisMessage implements IVesselPo
      * @return the rot
      */
     public int getRot() {
-        if (rot == 128) {
-            return 0;
-        }
-        int signedRot = (byte) rot;
-        double sensorRot = Math.pow(signedRot / 4.733, 2);
-        if (signedRot < 0) {
-            sensorRot *= -1;
-            return (int) sensorRot + 1;
-        } else {
-            return (int) sensorRot - 1;
-        }
+        return rot;
     }
 
     /**
